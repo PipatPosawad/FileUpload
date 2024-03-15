517b43674857ba4c8b643f9429fcc0c84ba62c40
Pipat Posawad <Pipat.P@draycir.com>
2024-03-15 11:40:15 +0700

    Add DI

---
+++ WebApi/DependencyInjection/ApiModule.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Services;
+using Service;
+
+namespace WebApi.DependencyInjection
+{
+    /// <summary>
+    /// Contains extension methods to <see cref="IServiceCollection"/> that adds API dependencies.
+    /// </summary>
+    public static class ApiModule
+    {
+        /// <summary>
+        /// Adds API dependencies to the service collection.
+        /// </summary>
+        /// <param name="services">A services collection.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddApiModule(this IServiceCollection services)
+        {
+            services.AddScoped<IFileUploadService, FileUploadService>();
+
+            return services;
+        }
+    }
+}
---
+++ WebApi/DependencyInjection/RepositoryModule.cs
@@ -0,0 +1,23 @@
+﻿using Domain.BlobRepositories;
+using Repository.BlobRepositories;
+
+namespace WebApi.DependencyInjection
+{
+    /// <summary>
+    /// Contains extension methods to <see cref="IServiceCollection"/> that adds repository dependencies.
+    /// </summary>
+    public static class RepositoryModule
+    {
+        /// <summary>
+        /// Adds repositories' dependencies to the service collection.
+        /// </summary>
+        /// <param name="services">A services collection.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddRepositoryModule(this IServiceCollection services)
+        {
+            services.AddScoped<IFileBlobRepository, FileBlobRepository>();
+
+            return services;
+        }
+    }
+}
