efe2765238fb54441c3d7b5913fba9b664e46bad
Pipat Posawad <evolution31@hotmail.com>
2024-03-16 21:46:24 +0700

    add EF

---
+++ Repository/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class EFContext : DbContext
+    {
+        public DbSet<FileEntity> Files { get; set; }
+    }
+}
