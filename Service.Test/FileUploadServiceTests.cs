2f14560b7a160727701e474c9da1a80c12f15837
Pipat Posawad <evolution31@hotmail.com>
2024-03-18 11:00:22 +0700

    add new test

--- Service.Test/FileUploadServiceTests.cs
+++ Service.Test/FileUploadServiceTests.cs
@@ -1,11 +1,36 @@
+using Domain.BlobRepositories;
+using Moq;
+
 namespace Service.Test
 {
     [TestClass]
     public class FileUploadServiceTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Mock<IFileBlobRepository> _mockFileBlobRepository;
+
+        private FileUploadService _service;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            _mockFileBlobRepository = new Mock<IFileBlobRepository>();
+
+            _service = new FileUploadService(_mockFileBlobRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task UploadAsync_ReturnsFileModel_WhenUploadingIsSuccessful()
+        {
+            // Arrange
+            var name = "";
+            var contentType = "";
+            using var stream = new MemoryStream();
+
+            // Act
+            var actual = await _service.UploadAsync(name, contentType, stream);
+
+            // Assert
+            Assert.IsNotNull(actual);
         }
     }
 }
