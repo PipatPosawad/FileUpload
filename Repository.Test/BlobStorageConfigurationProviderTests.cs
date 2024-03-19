f06706d5268aebbb6961ef3335b9d9b675c78737
Pipat Posawad <evolution31@hotmail.com>
2024-03-19 18:51:20 +0700

    fix test

--- Repository.Test/BlobStorageConfigurationProviderTests.cs
+++ Repository.Test/BlobStorageConfigurationProviderTests.cs
@@ -29,7 +29,7 @@ namespace DataAccess.Tests
         {
             // Arrange
             _mockStorageAccountConfiguration.Setup(x => x.ConnectionString)
-                .Returns("mock-connection-string");
+                .Returns("DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;");
 
             var uri = new Uri("https://localhost");
             _mockKeyVaultConfiguration.Setup(x => x.Uri)
